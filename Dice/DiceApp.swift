70e1361580885d5185b8024f271fae70b8f77a5a
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-08-26 16:26:11 -0400

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Kenji Dela Cruz on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
